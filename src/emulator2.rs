081a3ba044ee394395d7cf40daac36aad0e345e4
Cedric Beust <cedric@beust.com>
2021-09-20 15:33:21 -0700

    Better sharing model

--- src/emulator2.rs
+++ src/emulator2.rs
@@ -10,67 +10,67 @@ use emulator::opcodes::STA;
 use crate::log_time;
 use once_cell::sync::Lazy;
 
-static STATIC_MEMORY: Lazy<RwLock<Vec<u8>>> = Lazy::new(|| {
-    let m: Vec<u8> = vec![0; MEMORY_SIZE];
-    RwLock::new(m)
+struct State {
+    memory: Vec<u8>,
+    input_1: u8,
+}
+
+static SHARED_STATE: Lazy<RwLock<State>> = Lazy::new(|| {
+    RwLock::new(State {
+        memory: vec![0; MEMORY_SIZE],
+        input_1: 0,
+    })
 });
 
-struct EmulatorState<'a> {
-    memory: &'a Lazy<RwLock<Vec<u8>>>,
-}
-
-impl Clone for EmulatorState<'_> {
-    fn clone(&self) -> Self {
-        EmulatorState { memory: &STATIC_MEMORY }
-    }
-}
-
-trait Emulator2 {
-    fn graphic_memory(&self) -> Vec<u8>;
-    fn write_memory(&mut self, address: usize, value: u8);
+trait Emulator {
     fn run_one_frame(&self);
+    fn write_memory(&mut self, address: usize, value: u8);
+    fn memory(&self) -> Vec<u8>;
+    fn set_input_1(&mut self, value: u8);
+    fn input_1(&self) -> u8;
 }
+struct Runner{}
 
-impl EmulatorState<'_> {
-    fn new() -> Self {
-        EmulatorState { memory: &STATIC_MEMORY }
-    }
-}
-
-impl Emulator2 for EmulatorState<'_> {
-    fn graphic_memory(&self) -> Vec<u8> {
-        self.memory.read().unwrap().to_vec()
-        // let mut result: [u8; MEMORY_SIZE] = [0; MEMORY_SIZE];
-        // result.clone_from_slice(&_STATIC_MEMORY[0..MEMORY_SIZE]);
-        // result
+impl Emulator for Runner {
+    fn run_one_frame(&self) {
+        println!("Running one frame");
+        std::thread::sleep(Duration::from_millis(500));
     }
 
     fn write_memory(&mut self, address: usize, value: u8) {
-        println!("  WRITING {:04x} = {:02x}", address, value);
-        STATIC_MEMORY.write().unwrap()[address] = value;
+        SHARED_STATE.write().unwrap().memory[address] = value;
     }
 
-    fn run_one_frame(&self) {
-        println!("run_one_frame()");
-        std::thread::sleep(Duration::from_millis(500));
+    fn memory(&self) -> Vec<u8> {
+        SHARED_STATE.read().unwrap().memory.to_vec()
+    }
+
+    fn set_input_1(&mut self, value: u8) {
+        SHARED_STATE.write().unwrap().input_1 = value;
+    }
+
+    fn input_1(&self) -> u8 {
+        SHARED_STATE.read().unwrap().input_1
     }
 }
 
 pub(crate) fn main() {
-    let mut e2 = EmulatorState::new();
-    let mut e3 = EmulatorState::new();
+    let mut e2 = Runner{};
+    let mut e3 = Runner{};
     let t = thread::spawn(move || {
         let mut i = 0;
         loop {
             e2.run_one_frame();
+            println!("Writing {}, input_1: {}", i, e2.input_1());
             e2.write_memory(0, i);
             i += 1;
         }
     });
     loop {
-        let s = format!("============= Memory: {:02x}", e3.memory.read().unwrap()[0]);
+        let value = e3.memory()[0];
+        let s = format!("============= Memory: {:02x}", value);
+        e3.set_input_1(value);
         log_time(&s);
         thread::sleep(Duration::from_millis(500));
     }
-    t.join();
 }
