264fbfb238c8fd87b9591dbf37ec2e787ae440a6
Cedric Beust <cedric@beust.com>
2021-09-24 22:15:52 -0700

    Add wasm

--- wasm-space-invaders/src/lib.rs
+++ wasm-space-invaders/src/lib.rs
@@ -15,5 +15,5 @@ extern {
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, wasm-space-invaders!");
+    alert("Hello, Cedric invaders!");
 }
---
+++ wasm-space-invaders/www/index.js
@@ -0,0 +1,3 @@
+import * as wasm from "wasm-space-invaders";
+
+wasm.greet();
